dbec044e2ffe399c4e6bd8b386075bc1d48a21a7
Huynh Tien <huynhqtienvtag@gmail.com>
2021-10-19 15:28:50 +0700

    shorter with AttributeModifier for HealthSkill

--- src/main/java/com/leonardobishop/playerskills2/skill/HealthSkill.java
+++ src/main/java/com/leonardobishop/playerskills2/skill/HealthSkill.java
@@ -4,20 +4,18 @@ import com.leonardobishop.playerskills2.PlayerSkills;
 import com.leonardobishop.playerskills2.config.CreatorConfigValue;
 import com.leonardobishop.playerskills2.player.SPlayer;
 import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 
 public class HealthSkill extends Skill {
 
-    private final HashMap<UUID, Integer> hitpointsFromPlayerskills = new HashMap<>();
-    private final HashMap<UUID, Double> knownMaxHitpoints = new HashMap<>();
-    private final HashMap<UUID, Double> hitpoints = new HashMap<>();
+    private final Map<UUID, Integer> knownMaxHealth = new IdentityHashMap<>();
 
     public HealthSkill(PlayerSkills plugin) {
         super(plugin, "Health", "health");
@@ -31,109 +29,76 @@ public class HealthSkill extends Skill {
 
     @Override
     public void enable(PlayerSkills plugin) {
-        new BukkitRunnable() {
+        BukkitRunnable runnable = new BukkitRunnable() {
             @Override
             public void run() {
                 for (Player player : Bukkit.getOnlinePlayers()) {
-                    SPlayer sPlayer = SPlayer.get(player.getUniqueId());
+                    UUID uuid = player.getUniqueId();
+                    SPlayer sPlayer = SPlayer.get(uuid);
                     if (sPlayer == null) {
                         if (HealthSkill.super.getPlugin().isVerboseLogging()) {
-                            HealthSkill.super.getPlugin().logError("Failed event. SPlayer for " + player.getUniqueId() + " is null.");
+                            HealthSkill.super.getPlugin().logError("Failed event. SPlayer for " + uuid + " is null.");
                         }
                         continue;
                     }
                     if (isWorldNotAllowed(player)) {
+                        clearPlayer(player);
                         return;
                     }
-
-                    int hpNeeded = (sPlayer.getLevel(HealthSkill.super.getConfigName()) * (((int) HealthSkill.super.getConfig().get
-                            ("extra-health-per-level")) * 2));
-                    if (HealthSkill.super.getConfig().containsKey("compatibility-mode") &&
-                            (boolean) HealthSkill.super.getConfig().get("compatibility-mode")) {
-                        int hpApplied;
-                        if (!knownMaxHitpoints.containsKey(player.getUniqueId()) ||
-                                knownMaxHitpoints.get(player.getUniqueId()) != player.getMaxHealth()) {
-                            // maxhitpoints has changed from when playerskills altered it, assume health from playerskills
-                            // is 0
-                            hpApplied = 0;
-                        } else {
-                            hpApplied = hitpointsFromPlayerskills.getOrDefault(player.getUniqueId(), 0);
+                    int hpNeeded = (sPlayer.getLevel(HealthSkill.super.getConfigName()) * (((int) HealthSkill.super.getConfig().get("extra-health-per-level")) * 2));
+                    if (hpNeeded != knownMaxHealth.getOrDefault(uuid, 0)) {
+                        clearPlayer(player);
+                        if (hpNeeded > 0) {
+                            knownMaxHealth.put(player.getUniqueId(), hpNeeded);
+                            clearModifier(player);
+                            addNewHealth(player, hpNeeded);
                         }
-
-                        int hpDiff;
-                        if (hpApplied != hpNeeded) {
-                            hpDiff = hpNeeded - hpApplied;
-
-                            double newMaxHealth = player.getMaxHealth() + hpDiff;
-                            player.setMaxHealth(newMaxHealth);
-                            knownMaxHitpoints.put(player.getUniqueId(), newMaxHealth);
-                            hitpointsFromPlayerskills.put(player.getUniqueId(), hpNeeded);
-
-
-                            if (hitpoints.containsKey(player.getUniqueId())) {
-                                double health = hitpoints.get(player.getUniqueId());
-                                if (health > player.getMaxHealth()) {
-                                    player.setHealth(player.getMaxHealth());
-                                } else {
-                                    player.setHealth(health);
-                                }
-                            }
-                        }
-
-                        hitpoints.put(player.getUniqueId(), player.getHealth());
-                    } else {
-                        player.setMaxHealth(20 + hpNeeded);
                     }
-
                 }
             }
-        }.runTaskTimer(plugin, (HealthSkill.super.getConfig().containsKey("compatibility-mode") &&
-                        (boolean) HealthSkill.super.getConfig().get("compatibility-mode") ? 1L : 20L),
-                (HealthSkill.super.getConfig().containsKey("compatibility-mode") && (boolean) HealthSkill.super.getConfig().get("compatibility-mode") ? 1L : 20L));
+        };
+        long tick = (getConfig().containsKey("compatibility-mode") && (boolean) getConfig().get("compatibility-mode") ? 1L : 20L);
+        runnable.runTaskTimer(plugin, tick, tick);
+    }
+
+    private void addNewHealth(Player player, int amount) {
+        Optional.ofNullable(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).ifPresent(instance -> {
+            AttributeModifier modifier = new AttributeModifier("PlayerSkillsHealth", amount, AttributeModifier.Operation.ADD_NUMBER);
+            instance.addModifier(modifier);
+        });
+    }
+
+    private void clearPlayer(Player player) {
+        knownMaxHealth.remove(player.getUniqueId());
+        clearModifier(player);
+    }
+
+    private void clearModifier(Player player) {
+        Optional.ofNullable(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).ifPresent(instance -> {
+            for (AttributeModifier modifier : instance.getModifiers()) {
+                if (modifier.getName().equals("PlayerSkillsHealth")) {
+                    instance.removeModifier(modifier);
+                }
+            }
+        });
     }
 
     @EventHandler
-    public void onWorldSwitch(PlayerChangedWorldEvent event) {
-        Player player = event.getPlayer();
-        SPlayer sPlayer = SPlayer.get(player.getUniqueId());
-
-        if (isWorldNotAllowed(player)) {
-            return;
-        }
-
-        if (HealthSkill.super.getConfig().containsKey("compatibility-mode") && (boolean) HealthSkill.super.getConfig().get("compatibility-mode")) {
-            int hpApplied;
-            if (!knownMaxHitpoints.containsKey(player.getUniqueId()) || knownMaxHitpoints.get(player.getUniqueId()) != player.getMaxHealth()) {
-                // maxhitpoints has changed from when playerskills altered it, assume health from playerskills
-                // is 0
-                return;
-            } else {
-                hpApplied = hitpointsFromPlayerskills.getOrDefault(player.getUniqueId(), 0);
-            }
-
-            int hpNeeded = (sPlayer.getLevel(HealthSkill.super.getConfigName()) * (((int) HealthSkill.super.getConfig().get("extra-health-per-level")) * 2));
-
-            if (hpApplied == hpNeeded) {
-                player.setMaxHealth(player.getMaxHealth() - hpNeeded);
-            }
-
-            hitpoints.put(player.getUniqueId(), player.getHealth());
-        } else {
-            player.setMaxHealth(20);
-        }
+    public void onQuit(PlayerQuitEvent event) {
+        clearPlayer(event.getPlayer());
     }
 
     @Override
     public String getPreviousString(SPlayer player) {
         int healthLevel = player.getLevel(this.getConfigName());
-        int hp = healthLevel * ((int) HealthSkill.super.getConfig().get("extra-health-per-level"));
+        int hp = healthLevel * ((int) getConfig().get("extra-health-per-level"));
         return hp + (hp == 1 ? " heart" : " hearts");
     }
 
     @Override
     public String getNextString(SPlayer player) {
         int healthLevel = player.getLevel(this.getConfigName()) + 1;
-        int hp = healthLevel * ((int) HealthSkill.super.getConfig().get("extra-health-per-level"));
+        int hp = healthLevel * ((int) getConfig().get("extra-health-per-level"));
         return hp + (hp == 1 ? " heart" : " hearts");
     }
 }
