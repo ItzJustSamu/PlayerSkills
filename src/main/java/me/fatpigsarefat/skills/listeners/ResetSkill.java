9b88b9bc37cc0d21a420bc75f1f3508ce2bd8761
ItzJustSamu <45722897+ItzJustSamu@users.noreply.github.com>
2023-04-27 10:35:04 -0400

    Update ResetSkill.java

--- src/main/java/me/fatpigsarefat/skills/listeners/ResetSkill.java
+++ src/main/java/me/fatpigsarefat/skills/listeners/ResetSkill.java
@@ -5,6 +5,7 @@ import me.fatpigsarefat.skills.events.ResetSkillEvent;
 import me.fatpigsarefat.skills.helper.MessageHelper;
 import me.fatpigsarefat.skills.managers.SkillManager;
 import me.fatpigsarefat.skills.utils.Skill;
+import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,10 +20,19 @@ public class ResetSkill implements Listener {
         Skill skill = e.getSkill();
         if (!PlayerSkills.allowReset)
             return;
+
+        String version = Bukkit.getServer().getVersion();
+        if (version.contains("1.8") || version.contains("1.9")) {
+            player.playSound(player.getLocation(), Sound.ORB_PICKUP, 1.0F, 1.0F);
+        } else {
+            player.playSound(player.getLocation(), Sound.valueOf("ENTITY_EXPERIENCE_ORB_PICKUP"), 1.0F, 1.0F);
+        }
+
         sm.setSkillPoints(player, sm.getSkillPoints(player) + sm.getSkillLevel(player, skill) - 1);
         sm.setSkillLevel(player, skill, 1);
         InventoryClick.reconstructInventory(player, false);
         player.sendMessage(messageHelper.getMessage("skill_reset", new String[] { skill.name().toLowerCase() }));
-        player.playSound(player.getLocation(), Sound.ORB_PICKUP, 60.0F, 100.0F);
     }
 }
+
+
