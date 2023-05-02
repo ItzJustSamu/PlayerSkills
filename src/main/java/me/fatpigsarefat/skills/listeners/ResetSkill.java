74650cdd01b6d017e62ee23cf724e9dd2fd0ba53
ItzJustSamu <45722897+ItzJustSamu@users.noreply.github.com>
2023-05-01 22:28:29 -0400

    Update ResetSkill.java

--- src/main/java/me/fatpigsarefat/skills/listeners/ResetSkill.java
+++ src/main/java/me/fatpigsarefat/skills/listeners/ResetSkill.java
@@ -5,7 +5,6 @@ import me.fatpigsarefat.skills.events.ResetSkillEvent;
 import me.fatpigsarefat.skills.helper.MessageHelper;
 import me.fatpigsarefat.skills.managers.SkillManager;
 import me.fatpigsarefat.skills.utils.Skill;
-import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -20,12 +19,10 @@ public class ResetSkill implements Listener {
         Skill skill = e.getSkill();
         if (!PlayerSkills.allowReset)
             return;
-
-        String version = Bukkit.getServer().getVersion();
-        if (version.contains("1.8") || version.contains("1.9")) {
-            player.playSound(player.getLocation(), Sound.ORB_PICKUP, 1.0F, 1.0F);
-        } else {
-            player.playSound(player.getLocation(), Sound.valueOf("ENTITY_EXPERIENCE_ORB_PICKUP"), 1.0F, 1.0F);
+        try {
+            player.playSound(player.getLocation(), Sound.valueOf("ENTITY_GENERIC_EXPLODE"), 0.9F, 0.9F);
+        } catch (IllegalArgumentException ignored) {
+            player.playSound(player.getLocation(), Sound.valueOf("EXPLODE"), 0.9F, 0.9F);
         }
 
         sm.setSkillPoints(player, sm.getSkillPoints(player) + sm.getSkillLevel(player, skill) - 1);
