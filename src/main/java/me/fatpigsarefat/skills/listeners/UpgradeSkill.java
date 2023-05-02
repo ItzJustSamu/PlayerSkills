2afdaf5f0eaeb96c24317da949447471c68adeac
ItzJustSamu <45722897+ItzJustSamu@users.noreply.github.com>
2023-05-01 22:28:02 -0400

    Update UpgradeSkill.java

--- src/main/java/me/fatpigsarefat/skills/listeners/UpgradeSkill.java
+++ src/main/java/me/fatpigsarefat/skills/listeners/UpgradeSkill.java
@@ -6,13 +6,14 @@ import me.fatpigsarefat.skills.helper.MessageHelper;
 import me.fatpigsarefat.skills.managers.FileManager;
 import me.fatpigsarefat.skills.managers.SkillManager;
 import me.fatpigsarefat.skills.utils.Skill;
-import org.bukkit.Bukkit;
+
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class UpgradeSkill implements Listener {
+
     @EventHandler
     public void onUpgradeSkill(UpgradeSkillEvent e) {
         FileManager.Config config = PlayerSkills.getFileManager().getConfig("config");
@@ -20,83 +21,51 @@ public class UpgradeSkill implements Listener {
         SkillManager sm = e.getSkillManager();
         Skill skill = e.getSkill();
         MessageHelper messageHelper = new MessageHelper();
+
         if (config.get().getBoolean("permissions.use")) {
-            if (player.hasPermission("playerskills." + skill.name().toLowerCase())) {
-                if (config.get().getBoolean("permissions.level-perms") &&
-                        !player.hasPermission("playerskills." + skill.name() + "." + sm.getSkillLevel(player, skill))) {
-                    String version = Bukkit.getServer().getVersion();
-                    if (version.contains("1.8") || version.contains("1.9")) {
-                        player.playSound(player.getLocation(), Sound.ANVIL_LAND, 1.0F, 1.0F);
-                    } else {
-                        player.playSound(player.getLocation(), Sound.valueOf("BLOCK_ANVIL_LAND"), 1.0F, 1.0F);
-                    }
-                    player.sendMessage(messageHelper.getMessage("skill_upgrade_false_perms", new String[0]));
-                    return;
-                }
-                if (sm.getSkillPoints(player) <= 0) {
-                    String version = Bukkit.getServer().getVersion();
-                    if (version.contains("1.8") || version.contains("1.9")) {
-                        player.playSound(player.getLocation(), Sound.ANVIL_LAND, 1.0F, 1.0F);
-                    } else {
-                        player.playSound(player.getLocation(), Sound.valueOf("BLOCK_ANVIL_LAND"), 1.0F, 1.0F);
-                    }
-                    player.sendMessage(messageHelper.getMessage("skill_upgrade_false", new String[0]));
-                    return;
-                }
-                if (sm.getSkillLevel(player, skill) >= sm.getMaximumLevel(skill)) {
-                    String version = Bukkit.getServer().getVersion();
-                    if (version.contains("1.8") || version.contains("1.9")) {
-                        player.playSound(player.getLocation(), Sound.ANVIL_LAND, 1.0F, 1.0F);
-                    } else {
-                        player.playSound(player.getLocation(), Sound.valueOf("BLOCK_ANVIL_LAND"), 1.0F, 1.0F);
-                    }
-                    player.sendMessage(messageHelper.getMessage("skill_upgrade_false", new String[0]));
-                    return;
-                }
-                sm.setSkillPoints(player, sm.getSkillPoints(player) - 1);
-                sm.setSkillLevel(player, skill, sm.getSkillLevel(player, skill) + 1);
-                InventoryClick.reconstructInventory(player, false);
-                String version = Bukkit.getServer().getVersion();
-                if (version.contains("1.8") || version.contains("1.9")) {
-                    player.playSound(player.getLocation(), Sound.LEVEL_UP, 1.0F, 1.0F);
-                } else {
-                    player.playSound(player.getLocation(), Sound.valueOf("ENTITY_PLAYER_LEVELUP"), 1.0F, 1.0F);
-                }
-                player.sendMessage(messageHelper.getMessage("skill_upgrade", new String[] { skill.name().toLowerCase() }));
-            } else {
+            if (!player.hasPermission("playerskills." + skill.name().toLowerCase())) {
                 player.sendMessage(messageHelper.getMessage("no_permissions_message", new String[0]));
-            }
-        } else {
-            if (sm.getSkillPoints(player) <= 0) {
-                String version = Bukkit.getServer().getVersion();
-                if (version.contains("1.8") || version.contains("1.9")) {
-                    player.playSound(player.getLocation(), Sound.ANVIL_LAND, 1.0F, 1.0F);
-                } else {
-                    player.playSound(player.getLocation(), Sound.valueOf("BLOCK_ANVIL_LAND"), 1.0F, 1.0F);
+                return;
+            } else if (config.get().getBoolean("permissions.level-perms")
+                    && !player.hasPermission("playerskills." + skill.name() + "." + sm.getSkillLevel(player, skill))) {
+                try {
+                    player.playSound(player.getLocation(), Sound.valueOf("ANVIL_LAND"), 0.8F, 0.8F);
+                } catch (IllegalArgumentException ignored) {
+                    player.playSound(player.getLocation(), Sound.valueOf("BLOCK_ANVIL_LAND"), 0.8F, 0.8F);
                 }
-                player.sendMessage(messageHelper.getMessage("skill_upgrade_false", new String[0]));
+                player.sendMessage(messageHelper.getMessage("skill_upgrade_false_perms", new String[0]));
                 return;
             }
-            if (sm.getSkillLevel(player, skill) >= sm.getMaximumLevel(skill)) {
-                String version = Bukkit.getServer().getVersion();
-                if (version.contains("1.8") || version.contains("1.9")) {
-                    player.playSound(player.getLocation(), Sound.ANVIL_LAND, 1.0F, 1.0F);
-                } else {
-                    player.playSound(player.getLocation(), Sound.valueOf("BLOCK_ANVIL_LAND"), 1.0F, 1.0F);
-                }
-                player.sendMessage(messageHelper.getMessage("skill_upgrade_false", new String[0]));
-                return;
-            }
-            sm.setSkillPoints(player, sm.getSkillPoints(player) - 1);
-            sm.setSkillLevel(player, skill, sm.getSkillLevel(player, skill) + 1);
-            InventoryClick.reconstructInventory(player, false);
-            String version = Bukkit.getServer().getVersion();
-            if (version.contains("1.8") || version.contains("1.9")) {
-                player.playSound(player.getLocation(), Sound.ANVIL_LAND, 1.0F, 1.0F);
-            } else {
-                player.playSound(player.getLocation(), Sound.valueOf("BLOCK_ANVIL_LAND"), 1.0F, 1.0F);
-            }
-            player.sendMessage(messageHelper.getMessage("skill_upgrade", new String[] { skill.name().toLowerCase() }));
         }
+
+        if (sm.getSkillPoints(player) <= 0) {
+            try {
+                player.playSound(player.getLocation(), Sound.valueOf("ANVIL_LAND"), 0.8F, 0.8F);
+            } catch (IllegalArgumentException ignored) {
+                player.playSound(player.getLocation(), Sound.valueOf("BLOCK_ANVIL_LAND"), 0.8F, 0.8F);
+            }
+            player.sendMessage(messageHelper.getMessage("skill_upgrade_false", new String[0]));
+            return;
+        }
+
+        if (sm.getSkillLevel(player, skill) >= sm.getMaximumLevel(skill)) {
+            try {
+                player.playSound(player.getLocation(), Sound.valueOf("ANVIL_LAND"), 0.8F, 0.8F);
+            } catch (IllegalArgumentException ignored) {
+                player.playSound(player.getLocation(), Sound.valueOf("BLOCK_ANVIL_LAND"), 0.8F, 0.8F);
+            }
+            player.sendMessage(messageHelper.getMessage("skill_upgrade_limit", new String[] { skill.name().toLowerCase() }));
+            return;
+        }
+
+        sm.setSkillPoints(player, sm.getSkillPoints(player) - 1);
+        sm.setSkillLevel(player, skill, sm.getSkillLevel(player, skill) + 1);
+        InventoryClick.reconstructInventory(player, false);
+        try {
+            player.playSound(player.getLocation(), Sound.valueOf("LEVEL_UP"), 0.8F, 0.8F);
+        } catch (IllegalArgumentException ignored) {
+            player.playSound(player.getLocation(), Sound.valueOf("ENTITY_PLAYER_LEVELUP"), 0.8F, 0.8F);
+        }
+        player.sendMessage(messageHelper.getMessage("skill_upgrade", new String[] { skill.name().toLowerCase() }));
     }
 }
