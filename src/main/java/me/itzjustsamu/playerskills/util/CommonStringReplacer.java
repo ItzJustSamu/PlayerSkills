306bb72f6e805258b4d8d6e19d50f927aa285355
samu <ItzJustSamuel@gmail.com>
2024-01-07 18:28:28 -0500

    2.5.2 Menu Patches | Skills Increment testing

--- src/main/java/me/itzjustsamu/playerskills/util/CommonStringReplacer.java
+++ src/main/java/me/itzjustsamu/playerskills/util/CommonStringReplacer.java
@@ -9,7 +9,9 @@ import me.itzjustsamu.playerskills.skill.Skill;
 public final class CommonStringReplacer {
     private static Skill skill;
 
-
+    public static int getIncrement() {
+        return (skill != null) ? skill.getIncrement() : 0; // or any default value
+    }
     public static final StringReplacer COLORIZE = (original, uuid) -> ColorUtils.colorize(original);
     public static final StringReplacer PLAYER_PROPERTIES = (original, uuid) -> {
         SPlayer sPlayer = SPlayer.get(uuid);
@@ -20,7 +22,7 @@ public final class CommonStringReplacer {
                 .replace("{reset-points}", Integer.toString(MainConfig.POINTS_RESET_PRICE.getValue()))
                 .replace("{refund-status}", Boolean.toString(MainConfig.POINTS_REFUND_POINTS.getValue()))
                 .replace("{incremented-price}", Integer.toString(MainConfig.POINTS_INCREMENT_PRICE.getValue()))
-                .replace("{incremented-skill-price}", Integer.toString(skill.getIncrement()));
+                .replace("{incremented-skill-price}", Integer.toString(getIncrement()));
     };
 
 
